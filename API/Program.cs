d28486ea2c4e8759c6034eb8ed6c136dae5e892e
Saikumar kodari <saikdoari@gmail.com>
2023-01-09 22:04:42 -0500

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -2,7 +2,6 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -10,10 +9,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 var app = builder.Build();
-
-
-
+app.UseCors(builder =>builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
-
 app.Run();
