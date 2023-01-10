a26d811d54c0521ca0374f7179aa9080a5778569
Saikumar kodari <saikdoari@gmail.com>
2023-01-10 18:33:19 -0500

    End of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] //GET /api/users
-    public class UsersController : ControllerBase
+   [Authorize]
+    public class UsersController : BaseApiController
     {
 
         private readonly DataContext _context;
@@ -24,6 +24,7 @@ namespace API.Controllers
             var users = await _context.Users.ToListAsync();
             return users;
         }
+        
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
